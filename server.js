b32d63e21150663b261bd0b594b1e3bb60acfb96
Harsukhjit Singh <230235494@aston.ac.uk>
2025-04-23 03:12:53 +0100

    Fix: listen on 0.0.0.0 for Render

--- server.js
+++ server.js
@@ -571,6 +571,7 @@ if (!uid) {
 
 // Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
